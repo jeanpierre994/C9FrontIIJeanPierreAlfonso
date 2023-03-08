08dc5198208869349bd21555ac170c180cfaaa98
Jean Pierre Alfonso <Jeanpierre994@gmail.com>
2023-03-08 18:16:06 -0500

    preba con css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Testimonio from './Componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      
-      <Testimonio/>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        
+        <Testimonio/>
+      </div>
     </div>
   );
 }
